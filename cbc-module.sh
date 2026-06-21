ae55b5252b273df836b56604c271b741a68f9d59
iop098321qwe <iop098321qwe@pm.me>
2026-06-20 21:51:15 -0600

    chore(pretty): add `pretty` function scaffold

--- cbc-module.sh
+++ cbc-module.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+function pretty() {
+  # write a pretty command launcher like `pretty html` or `pretty json` to launch prettier via npx on specific file types. 
+  # Can use `pretty web` to launch prettier on all web related file types (html, css, js, json, etc.)
+  pass
+}
